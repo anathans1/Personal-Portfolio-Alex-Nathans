b5a0f98a6327238f84fd31ff0ab3d243dc33e649
anathans1 <alexnathans1993@gmail.com>
2022-08-11 13:57:11 -0400

    changed background layout and added projects seperate pages

--- js/app.js
+++ js/app.js
@@ -8,3 +8,29 @@ const closeBtn = document.querySelector(".close-modal");
 closeBtn.addEventListener("click", () => {
   modal.classList.remove("active")
 })
+
+(function(){
+  $('.flex-container').waitForImages(function() {
+    $('.spinner').fadeOut();
+}, $.noop, true);
+
+$(".flex-slide").each(function(){
+  $(this).hover(function(){
+    $(this).find('.flex-title').css({
+      transform: 'rotate(0deg)',
+      top: '10%'
+    });
+    $(this).find('.flex-about').css({
+      opacity: '1'
+    });
+  }, function(){
+    $(this).find('.flex-title').css({
+      transform: 'rotate(90deg)',
+      top: '15%'
+    });
+    $(this).find('.flex-about').css({
+      opacity: '0'
+    });
+  })
+});
+})();
